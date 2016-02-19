2b8865f4e27318cf5d04d17a2d10ab6a0ddace16
gj <JJWorld@gj.com>
2016-02-20 00:46:09 +0800

    '第七次-添加导航焦点状态切换'

--- app.js
+++ app.js
@@ -7,4 +7,21 @@ angular.module('moviecat', [
 ]).
 config(['$routeProvider', function($routeProvider) {
     $routeProvider.otherwise({ redirectTo: '/in_theaters/1' });
-}]);
+}]).controller('NavController', [
+    '$scope',
+    '$location',
+    function($scope, $location) {
+        //变量本地化
+        $scope.$location = $location;
+        $scope.$watch('$location.path()', function(now) {
+            if (now.startsWith('/in_theaters')) {
+                $scope.type = 'in_theaters';
+            } else if (now.startsWith('/coming_soon')) {
+                $scope.type = 'coming_soon';
+            } else if (now.startsWith('/top250')) {
+                $scope.type = 'top250';
+            }
+        })
+
+    }
+]);
