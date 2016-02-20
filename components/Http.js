e7a114f68a94c2849a1413f1026b0f9f34f18cdc
gj <JJWorld@gj.com>
2016-02-20 23:52:36 +0800

    '第十次-jsonp回调'

--- components/Http.js
+++ components/Http.js
@@ -2,7 +2,7 @@
  * @Author: sk
  * @Date:   2016-02-18 23:00:22
  * @Last Modified by:   sk
- * @Last Modified time: 2016-02-18 23:55:07
+ * @Last Modified time: 2016-02-20 23:48:56
  */
 
 (function(angular) {
@@ -12,17 +12,27 @@
     var http = angular.module('moviecat.services.http', []);
     http.service('HttpService', ['$window', '$document', function($window, $document) {
         this.jsonp = function(url, data, callback) {
-            var fnSuffix = Math.random().toString().replace('.', '');
-            var cbFuncName = 'my_json_cb_' + fnSuffix;
-            $window[cbFuncName] = callback;
+
             var queryString = url.indexOf('? ') == -1 ? '?' : '&';
             for (var key in data) {
                 queryString += key + '=' + data[key] + '&';
+            }
+            //挂靠全局函数名操作
+            var fnSuffix = Math.random().toString().replace('.', '');
+            var cbFuncName = 'my_json_cb_' + fnSuffix;
+            $window[cbFuncName] = function(data) {
+                callback(data);
+                $document[0].body.removeChild(scriptElement);
             }
             queryString += 'callback=' + cbFuncName;
             var scriptElement = $document[0].createElement('script');
             scriptElement.src = url + queryString;
             $document[0].body.appendChild(scriptElement);
+
+            //01.根据异步请求的地址拼接一个callback参数
+            //02.将这个地址当做一个script文件请求
+            //callback(data);
+            //调用完之后把自己干掉
         };
 
     }]);
--- components/auto-focus.js
+++ components/auto-focus.js
@@ -2,7 +2,7 @@
  * @Author: sk
  * @Date:   2016-02-20 21:51:04
  * @Last Modified by:   sk
- * @Last Modified time: 2016-02-20 22:50:28
+ * @Last Modified time: 2016-02-20 22:54:27
  */
 
 (function(angular) {
@@ -21,16 +21,14 @@
                         var aLink = iElm.children().attr('href');
                         var type = aLink.replace(/#(\/.+?)\/\d+/, '$1');
                         if (now.startsWith(type)) {
+                            iElm.parent().children().removeClass('active');
+                            iElm.addClass('active');
                             //访问的是当前链接
                             iElm.addClass('active');
                         }
-                        iElm.on('click', function() {
-                            iElm.parent().children().removeClass('active');
-                            iElm.addClass('active');
-                            //开发小技巧--看一下某个对象上有哪些成员
-                            //window.iele = iElm;
 
-                        })
+                        //开发小技巧--看一下某个对象上有哪些成员
+                        //window.iele = iElm;
                     })
 
                 }
